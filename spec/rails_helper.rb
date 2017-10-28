b4cadc9cfdf3d88b91b65e3379cdcfe6febf3f7d
Pedro Tanaka <pedro.stanaka@gmail.com>
2017-10-28 21:08:51 -0200

    Fixing FactoryBot and Shoulda integration with Rspec.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,8 +29,16 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+
+    with.library :rails
+  end
+end
+
 RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
   config.include RequestSpecHelper
   config.include ControllerSpecHelper
 
