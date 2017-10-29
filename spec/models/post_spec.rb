f83b51cda30c80b91caf535ff4a98e2d359d0946
Pedro Tanaka <pedro.stanaka@gmail.com>
2017-10-29 00:26:30 -0200

    Fixing rubocop offenses.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
 
   it { should(validate_presence_of :title) }
-  it { should(validate_presence_of :body ) }
+  it { should(validate_presence_of :body) }
 
 end
